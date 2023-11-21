398f40ec45618e91ded7f8ca50e7d341b756a432
AlfonsoNango <molina.nango@gmail.com>
2023-11-21 17:45:07 -0600

    add new images

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,15 +1,4 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}];
-
 let productsHTML = '';
-
 
 products.forEach((product) => {
     productsHTML += `
